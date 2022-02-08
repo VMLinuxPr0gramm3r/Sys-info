97a3ace6d056cb841b5b60df4a6bd854c496c57d
VMLinuxPr0gramm3r <99178290+VMLinuxPr0gramm3r@users.noreply.github.com>
2022-02-08 20:43:52 +0530

    Added Private IP adress support

--- src/main.c
+++ src/main.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <netinet/in.h>
-#include <net/if.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 
 #include "gethostname.h"
 #include "os-detector.h"
+#include "private-ip.h"
 
 #define SYSTEM_INFORMATION
 
