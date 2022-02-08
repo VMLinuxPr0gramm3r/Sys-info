540e0ad7f777ebcd7ae0a9b115dfd3e157889be8
VMLinuxPr0gramm3r <99178290+VMLinuxPr0gramm3r@users.noreply.github.com>
2022-02-08 18:33:16 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,26 +10,9 @@
 #include <arpa/inet.h>
 
 #include "gethostname.h"
+#include "os-detector.h"
 
 #define SYSTEM_INFORMATION
-
-void os_detector(){
-    #ifdef _WIN32
-        printf("OS: Windows 32-bit\n");
-    #endif
-    #ifdef _WIN64
-        printf("OS: Windows 64-bit\n");
-    #endif
-    #ifdef TARGET_OS_MAC
-        printf("OS: Mac OS X\n");
-    #endif
-    #ifdef __linux__
-        printf("OS: Linux\n");
-    #endif
-    #ifdef __unix__
-        printf("OS: UNIX\n");
-    #endif
-}
 
 int private_ip_list(){
     int n;
